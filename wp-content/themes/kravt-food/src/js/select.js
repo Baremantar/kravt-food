caf7d6cdc935b0aad637a1bae6254cd2137ec4d5
Timur <baremantar@gmail.com>
2022-08-15 12:45:47 +0300

    some visual changes + comments with mobile version

--- wp-content/themes/kravt-food/src/js/select.js
+++ wp-content/themes/kravt-food/src/js/select.js
@@ -1,13 +1,14 @@
-import {forEach} from "@splidejs/splide/src/js/utils";
-
 function select() {
     let list = document.getElementsByClassName('select-days')
     for (let item of list) {
         item.addEventListener('click', (element) => {
+            element.target.getElementsByTagName('svg')[0].classList.toggle('active');
             if (element.target.classList.contains('select-days')) {
-            element.target.classList.toggle('active')
-            }                                              else {
+                element.target.classList.toggle('active');
+
+            } else {
                 element.target.parentNode.classList.toggle('active')
+
             }
         })
     }
--- wp-content/themes/kravt-food/views/home.php
+++ wp-content/themes/kravt-food/views/home.php
@@ -8,25 +8,26 @@ Template Name: Homepage
 get_header();
 ?>
 <main class="homepage">
-	<?php include( get_stylesheet_directory() . '/components/home/slider.php' ) ?>
-	<?php include( get_stylesheet_directory() . '/components/home/benefits.php' ) ?>
-	<?php include( get_stylesheet_directory() . '/components/home/calculator.php' ) ?>
-	<?php include( get_stylesheet_directory() . '/components/home/programs.php' ) ?>
-	<?php include( get_stylesheet_directory() . '/components/home/nutrition.php' ) ?>
-	<?php include( get_stylesheet_directory() . '/components/home/menus.php' ) ?>
-	<?php include( get_stylesheet_directory() . '/components/home/offer.php' ) ?>
-	<?php include( get_stylesheet_directory() . '/components/home/specialists.php' ) ?>
-	<?php include( get_stylesheet_directory() . '/components/home/standarts.php' ) ?>
-	<?php include( get_stylesheet_directory() . '/components/home/personal_diet.php' ) ?>
-	<?php include( get_stylesheet_directory() . '/components/home/confidence.php' ) ?>
-	<?php include( get_stylesheet_directory() . '/components/home/faq.php' ) ?>
-	<?php include( get_stylesheet_directory() . '/components/home/uncertainty.php' ) ?>
-	<?php include( get_stylesheet_directory() . '/components/home/delivery.php' ) ?>
-	<?php include( get_stylesheet_directory() . '/components/home/payments.php' ) ?>
-	<?php include( get_stylesheet_directory() . '/components/home/which_program.php' ) ?>
-  	<?php include( get_stylesheet_directory() . '/components/home/banner.php' ) ?>
-  	<?php include( get_stylesheet_directory() . '/components/support.php' ) ?>
+	<?php
+  include( get_stylesheet_directory() . '/components/home/slider.php' );
+	include( get_stylesheet_directory() . '/components/home/benefits.php' );
+	include( get_stylesheet_directory() . '/components/home/calculator.php' );
+	include( get_stylesheet_directory() . '/components/home/programs.php' );
+	include( get_stylesheet_directory() . '/components/home/nutrition.php' );
+	include( get_stylesheet_directory() . '/components/home/menus.php' );
+	include( get_stylesheet_directory() . '/components/home/offer.php' );
+	include( get_stylesheet_directory() . '/components/home/specialists.php' );
+	include( get_stylesheet_directory() . '/components/home/standarts.php' );
+	include( get_stylesheet_directory() . '/components/home/personal_diet.php' );
+	include( get_stylesheet_directory() . '/components/home/confidence.php' );
+	include( get_stylesheet_directory() . '/components/home/faq.php' );
+	include( get_stylesheet_directory() . '/components/home/uncertainty.php' );
+	include( get_stylesheet_directory() . '/components/home/delivery.php' );
+	include( get_stylesheet_directory() . '/components/home/payments.php' );
+	include( get_stylesheet_directory() . '/components/home/which_program.php' );
+	include( get_stylesheet_directory() . '/components/home/banner.php' );
+	include( get_stylesheet_directory() . '/components/support.php' ) ?>
 </main>
 <?php
-get_footer();
+ get_footer();
 ?>                                                
